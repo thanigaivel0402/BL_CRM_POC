9c26e2fd3f221e465cbe2144ec51ef4abbe2785e
Rthanigaivel <dev.thanigaivel@gmail.com>
2025-10-23 17:48:31 +0530

    Dashboard screen added

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,13 +1,13 @@
+import 'package:bl_crm_poc_app/pages/dashboard_page.dart';
 import 'package:bl_crm_poc_app/pages/splash_page.dart';
 import 'package:go_router/go_router.dart';
 
-class AppRoutes{
+class AppRoutes {
   static final routes = GoRouter(
-    initialLocation: '/',
+    initialLocation: '/dashboard',
     routes: [
-    GoRoute(path: '/', builder: (context, state) => SplashPage(),),
-    
-  ] );
-    
-  
+      GoRoute(path: '/', builder: (context, state) => SplashPage()),
+      GoRoute(path: '/dashboard', builder: (context, state) => DashboardPage()),
+    ],
+  );
 }
